fbcb725d07cff484f571ee8d5fc0a22b71c5d8b5
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-04-20 20:19:55 -0500

    b4 last buttons

--- script.js
+++ script.js
@@ -3,11 +3,66 @@ let lat;
 let lon;
 let cityName;
 
+let now = dayjs();
+let today = dayjs().format("MM/DD/YYYY");
+let tomorrow = now.add(1, "day");
+
+let storageParse;
+let btnCol;
+let btn;
+
+function pullStorage() {
+  storageParse = JSON.parse(localStorage.getItem("previousSearch"));
+}
+
+function checkStorage() {
+  if (storageParse === null) {
+    storageParse = [];
+  }
+  if (!storageParse.includes(cityName)) {
+    storageParse.unshift(cityName);
+    if (storageParse.length > 5) {
+      storageParse.pop();
+    }
+    localStorage.setItem("previousSearch", JSON.stringify(storageParse));
+  }
+}
+
+function createBtns() {
+  btnCol = $(".sidebar-bottom");
+  $(btnCol).empty();
+  for (let i = 0; i < storageParse.length && i < 6; i++) {
+    btn = $(
+      `<button class="previousBtns" id="previousBtns${i}">${storageParse[
+        i
+      ].toLowerCase()}</button>`
+    );
+
+    if (i % 2 === 0) {
+      console.log("#previousBtns" + i);
+      btn.css("background-color", "lightgrey");
+    }
+    btnCol.append(btn);
+    /*  */
+    /*  */
+    /*  */
+    /* THIS IS WHAT U NEED HELP WITH RIGHT HERE */
+  }
+}
+
+function storage() {
+  pullStorage();
+  checkStorage();
+  createBtns();
+  console.log("storage");
+}
+
 async function searchByCity() {
   cityName = $("#searchbar").val();
+
   const url1 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}
 `;
-  console.log(url1);
+
   try {
     const response = await fetch(url1);
     const data = await response.json();
@@ -35,22 +90,12 @@ async function searchByLatLon(lat, lon) {
     createCards(data);
     mainForecast(data);
   } catch (error) {
-    console.log("error");
     console.log(error);
   }
 }
 
-async function weatherForecast() {
-  const { lat, lon } = await searchByCity();
-  searchByLatLon(lat, lon);
-}
-
-let now = dayjs();
-let today = dayjs().format("MM/DD/YYYY");
-let tomorrow = now.add(1, "day");
-console.log(tomorrow.format("dddd, MMMM D, YYYY"));
-
 function mainForecast(data) {
+  $(".section-top").empty();
   let temp = (((data.daily[0].temp.day - 273.15) * 9) / 5 + 32).toFixed(2);
 
   let forecastCard = $(".section-top");
@@ -58,19 +103,13 @@ function mainForecast(data) {
   <div class="card-body" id="main-forecast">
   <h2 class="forcast-header">${cityName} (${today})</h2>
   <h3>${now.format("dddd")}</h3>
-    
-  
-    <img src="https://openweathermap.org/img/wn/${
-      data.daily[0].weather[0].icon
-    }@2x.png" alt="..." style="width: 65px"/>
-    <br /><br />
-   <p class="card-info"><b>Temp:</b>&nbsp&nbsp${temp} F</p>
-   <p class="card-info"><b>Wind:</b>&nbsp&nbsp${
-     data.daily[0].wind_speed
-   } MPH</p>
-   <p class="card-info"><b>Humidity:</b>&nbsp&nbsp${
-     data.daily[0].humidity
-   } %</p>
+  <img src="https://openweathermap.org/img/wn/${
+    data.daily[0].weather[0].icon
+  }@2x.png" alt="..." style="width: 65px"/>
+  <br /><br />
+  <p class="card-info"><b>Temp:</b>&nbsp&nbsp${temp} F</p>
+  <p class="card-info"><b>Wind:</b>&nbsp&nbsp${data.daily[0].wind_speed} MPH</p>
+  <p class="card-info"><b>Humidity:</b>&nbsp&nbsp${data.daily[0].humidity} %</p>
   </div>
   `;
 
@@ -78,32 +117,37 @@ function mainForecast(data) {
 }
 
 function createCards(data) {
+  $(".card-container").empty();
   for (let i = 1; i < 6; i++) {
     let temp = (((data.daily[i].temp.day - 273.15) * 9) / 5 + 32).toFixed(2);
-    let cardDate = now.add(i + 1, "day");
+    let cardDate = now.add(i, "day");
 
-    console.log(data);
-
-    console.log("card working");
+    /* console.log(data); */
     let cardRow = $(".card-container");
     let cardHtml = `<div class="card">
-<div class="card-body">
-<h3>${cardDate.format("dddd")}</h3>
-  <h3>${cardDate.format("MM/DD/YYYY")}</h3>
-  <br />
 
+    <div class="card-body">
+    <h3>${cardDate.format("dddd")}</h3>
+    <h3>${cardDate.format("MM/DD/YYYY")}</h3>
+  <br />
   <img src="https://openweathermap.org/img/wn/${
     data.daily[i].weather[0].icon
   }@2x.png" class="card-img-top" alt="..." style="width: 65px"/>
   <br />
- <p class="card-info"><b>Temp:</b>&nbsp&nbsp${temp} F</p>
- <p class="card-info"><b>Wind:</b>&nbsp&nbsp${data.daily[i].wind_speed} MPH</p>
- <p class="card-info"><b>Humidity:</b>&nbsp&nbsp${data.daily[i].humidity} %</p>
-</div>
-</div>`;
+  <p class="card-info"><b>Temp:</b>&nbsp&nbsp${temp} F</p>
+  <p class="card-info"><b>Wind:</b>&nbsp&nbsp${data.daily[i].wind_speed} MPH</p>
+  <p class="card-info"><b>Humidity:</b>&nbsp&nbsp${data.daily[i].humidity} %</p>
+  </div>
+  </div>`;
 
     cardRow.append(cardHtml);
   }
+}
+
+async function weatherForecast() {
+  const { lat, lon } = await searchByCity();
+  searchByLatLon(lat, lon);
+  storage();
 }
 
 $("#submitBtn").click(weatherForecast);
